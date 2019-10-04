c97f5742d45507c659179ab5b59d9a24216548ed
Mateusz Kochanek <mkochanek@pl.sii.eu>
2019-10-04 14:37:21 +0200

    Remove obsolete Magento development environment

--- install-mage-2.2.sh
+++ install-mage-2.2.sh
@@ -5,37 +5,37 @@ docker-compose exec php composer install
 docker-compose exec php ./bin/magento maintenance:enable
 
 docker-compose exec php ./bin/magento setup:install \
-    --backend-frontname='admin' \
-    --db-host='127.0.0.1' \
-    --db-name='magento' \
-    --db-user='magento' \
-    --db-password='magento' \
-    --session-save='redis' \
-    --session-save-redis-host='127.0.0.1' \
-    --session-save-redis-db='0' \
-    --cache-backend='redis' \
-    --cache-backend-redis-server='127.0.0.1' \
-    --cache-backend-redis-db='1' \
-    --page-cache='redis' \
-    --page-cache-redis-server='127.0.0.1' \
-    --page-cache-redis-db='2' \
-    --admin-firstname='Magento' \
-    --admin-lastname='Admin' \
-    --admin-email='admin@example.com' \
-    --admin-user='admin' \
-    --admin-password='Qwerty123'
+  --backend-frontname='admin' \
+  --db-host='127.0.0.1' \
+  --db-name='magento' \
+  --db-user='magento' \
+  --db-password='magento' \
+  --session-save='redis' \
+  --session-save-redis-host='127.0.0.1' \
+  --session-save-redis-db='0' \
+  --cache-backend='redis' \
+  --cache-backend-redis-server='127.0.0.1' \
+  --cache-backend-redis-db='1' \
+  --page-cache='redis' \
+  --page-cache-redis-server='127.0.0.1' \
+  --page-cache-redis-db='2' \
+  --admin-firstname='Magento' \
+  --admin-lastname='Admin' \
+  --admin-email='admin@example.com' \
+  --admin-user='admin' \
+  --admin-password='Qwerty123'
 
 docker-compose exec php ./bin/magento maintenance:enable
 docker-compose exec php ./bin/magento deploy:mode:set developer
 
-docker-compose exec php ./bin/magento config:set --lock-config dev/debug/debug_logging 1
-docker-compose exec php ./bin/magento config:set --lock-config web/unsecure/base_url 'http://magento.localhost/'
-docker-compose exec php ./bin/magento config:set --lock-config web/seo/use_rewrites 1
-docker-compose exec php ./bin/magento config:set --lock-config web/url/use_store 1
-docker-compose exec php ./bin/magento config:set --lock-config system/full_page_cache/caching_application '2'
-docker-compose exec php ./bin/magento config:set --lock-config system/full_page_cache/varnish/access_list '127.0.0.1'
-docker-compose exec php ./bin/magento config:set --lock-config system/full_page_cache/varnish/backend_host '127.0.0.1'
-docker-compose exec php ./bin/magento config:set --lock-config system/full_page_cache/varnish/backend_port '8080'
+docker-compose exec php ./bin/magento config:set --lock-env dev/debug/debug_logging 1
+docker-compose exec php ./bin/magento config:set --lock-env web/unsecure/base_url 'http://magento.localhost/'
+docker-compose exec php ./bin/magento config:set --lock-env web/seo/use_rewrites 1
+docker-compose exec php ./bin/magento config:set --lock-env web/url/use_store 1
+docker-compose exec php ./bin/magento config:set --lock-env system/full_page_cache/caching_application '2'
+docker-compose exec php ./bin/magento config:set --lock-env system/full_page_cache/varnish/access_list '127.0.0.1'
+docker-compose exec php ./bin/magento config:set --lock-env system/full_page_cache/varnish/backend_host '127.0.0.1'
+docker-compose exec php ./bin/magento config:set --lock-env system/full_page_cache/varnish/backend_port '8080'
 
 docker-compose exec php ./bin/magento setup:config:set --http-cache-hosts=127.0.0.1:80
 
